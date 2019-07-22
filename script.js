117364636639b307826a54edce8a2ec4e81e0c33
shnurok <shnurok-98@mail.ru>
2019-07-22 22:12:56 +0300

    add script

---
+++ script.js
@@ -0,0 +1,18 @@
+let arr = [
+	{
+		todo: 'Something to note',
+		completed: false
+	},
+	{
+		todo: 'Something',
+		completed: false
+	},
+	{
+		todo: 'note',
+		completed: true
+	}
+];
+
+let todoList = document.querySelector('.todo-list');
+
+console.log(todoList);
